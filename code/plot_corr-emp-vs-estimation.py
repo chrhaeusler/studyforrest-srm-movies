b8b1aedd5dcd7260be87297a2262a21ac35a5025
Christian O. Häusler <der.haeusler@gmx.net>
2023-01-18 18:05:39 +0100

    add possibility to put boxplot under stripplot

--- code/plot_corr-emp-vs-estimation.py
+++ code/plot_corr-emp-vs-estimation.py
@@ -4,7 +4,6 @@ created on Mon March 29th 2021
 author: Christian Olaf Haeusler
 '''
 
-from glob import glob
 import argparse
 import ipdb
 import matplotlib.pyplot as plt
@@ -12,6 +11,12 @@ import numpy as np
 import os
 import pandas as pd
 import seaborn as sns
+
+STIMULICOLORS = {'anatomical alignment': 'grey',
+                 'visual localizer': 'limegreen',
+                 'movie': 'red',
+                 'audio-description': 'blue'
+                 }
 
 
 def parse_arguments():
@@ -23,17 +28,22 @@ def parse_arguments():
     parser.add_argument('-invis',
                         required=False,
                         default='test/corr_vis-ppa-vs-estimation_srm-ao-av-vis_feat10.csv',
-                        help='the data as csv')
+                        help='csv file with correlations VIS vs. estimation')
 
     parser.add_argument('-inav',
                         required=False,
                         default='test/corr_av-ppa-vs-estimation_srm-ao-av-vis_feat10.csv',
-                        help='the data as csv')
+                        help='csv file with correlations AV vs. estimation')
 
     parser.add_argument('-inao',
                         required=False,
                         default='test/corr_ao-ppa-vs-estimation_srm-ao-av-vis_feat10.csv',
-                        help='the data as csv')
+                        help='csv file with correlations AO vs. estimation')
+
+    parser.add_argument('-incronb',
+                        required=False,
+                        default='test/statistics_cronbachs.csv',
+                        help='csv file with Cronbachs Alpha of PPA from VIS, AV & AO')
 
     parser.add_argument('-outdir',
                         required=False,
@@ -45,62 +55,90 @@ def parse_arguments():
     inVisResults = args.invis
     inAvResults = args.inav
     inAoResults = args.inao
+    inCronbachs = args.incronb
     outdir = args.outdir
 
-    return inVisResults, inAvResults, inAoResults, outdir
+    return inVisResults, inAvResults, inAoResults, inCronbachs, outdir
 
 
-def find_files(pattern):
+def plot_boxplot(axis, df):
     '''
     '''
-    def sort_nicely(l):
-        '''Sorts a given list in the way that humans expect
-        '''
-        convert = lambda text: int(text) if text.isdigit() else text
-        alphanum_key = lambda key: [convert(c) for c in re.split("([0-9]+)", key)]
-        l.sort(key=alphanum_key)
+    axis = sns.boxplot(
+        data=df,
+        ax=axis,
+        zorder=2,  # boxplots are supposed to be behind the stripplot
+        x='number of runs',
+        y="Pearson's r",
+        hue='prediction via',
+        hue_order=STIMULICOLORS.keys(),
+        palette=STIMULICOLORS,
+        # median
+        medianprops={'visible': True, 'color': 'k'},
+        # mean
+        showmeans=True,
+        meanline=True,
+        meanprops={'color': 'k', 'ls': '--', 'lw': 1, 'alpha': 0.3},
+        # outliers
+        showfliers=True,
+        flierprops={'marker': 'x', 'markersize': 8},
+        # box
+        showbox=True,
+        boxprops={'alpha': 0.3},
+        # whiskers
+        whiskerprops={'visible': True, 'alpha': 0.3},
+        # caps (horizontal lines at the ends of whiskers)
+        showcaps=True,
+        capprops={'alpha': 0.3},
+    )
 
-        return l
+    # remove the handles and labels from the legend
+    # handles, labels = axis.get_legend_handles_labels()
+    axis.legend([], [], loc='lower right')
 
-    found_files = glob(pattern)
-    found_files = sort_nicely(found_files)
-
-    return found_files
+    return axis
 
 
-def plot_subplot(axNr, title, df, legend=True):
+def plot_subplot(axis, title, df, boxplot=False, legend=True):
     '''
     '''
-    axes[axNr].set_title(title)
+    axis.set_title(title)
 
-    ax = sns.stripplot(ax=axes[axNr],
-                       x='number of runs',
-                       y="Pearson's r",
-                       hue='prediction via',
-                       hue_order=['anatomical alignment',
-                                  'visual localizer',
-                                  'movie',
-                                  'audio-description'
-                                  ],
-                       palette = {'anatomical alignment' : 'grey',
-                                  'visual localizer' : 'limegreen',
-                                  'movie' : 'red',
-                                  'audio-description' : 'blue'
-                                  },
-                       jitter=0.2,
-                       linewidth=1,
-                       dodge=True,
-                       data=df)
+    if boxplot is True:
+        axis = plot_boxplot(axis, df)
 
-    if not legend:
-        ax.legend().remove()
+    axis = sns.stripplot(
+        data=df,
+        ax=axis,
+        x='number of runs',
+        y="Pearson's r",
+        hue='prediction via',
+        hue_order=STIMULICOLORS.keys(),
+        palette=STIMULICOLORS,
+        jitter=0.2,
+        linewidth=1,
+        dodge=True
+    )
 
-    return ax
+    # set the same limits for the y axis
+    # to make subplots comparable
+    axis.set_ylim([-0.3, 1])
+    axis.set_yticks([-0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0])
+
+    # one legend in the figure is sufficient
+    # remove it from (some) subplots
+    if legend:
+        handles, labels = axis.get_legend_handles_labels()
+        axis.legend(handles[-4:], labels[-4:], loc='lower right')
+    else:
+        axis.legend().remove()
+
+    return axis
 
 
 if __name__ == "__main__":
     # read command line arguments
-    visResults, avResults, aoResults, outDir = parse_arguments()
+    visResults, avResults, aoResults, cronbachs, outDir = parse_arguments()
 
     # some preparations for plotting
     # close figure
@@ -116,34 +154,7 @@ if __name__ == "__main__":
     # figure title
     fig.suptitle('Correlations between empirical and predicted Z-maps')
 
-    # upper subplot: visual localizer
-    # read the data first
-    visDf = pd.read_csv(visResults)
-    axNr = 0
-    axes[axNr] = plot_subplot(axNr,
-                              'PPA localized via visual localizer (Sengupta et al., 2016)',
-                              visDf,
-                              legend=True
-                              )
-
-    # plot middle subplot
-    avDf = pd.read_csv(avResults)
-    axNr = 1
-    axes[axNr] = plot_subplot(axNr,
-                              'PPA localized via movie (Häusler et al., 2022)',
-                              avDf,
-                              legend=False
-                              )
-
-    # plot lower subplot
-    aoDf = pd.read_csv(aoResults)
-    axNr = 2
-    axes[axNr] = plot_subplot(axNr,
-                              'PPA localized via audio-description (Häusler et al., 2022)',
-                              aoDf,
-                              legend=False
-                              )
-
+    # adjust some spacings
     plt.subplots_adjust(hspace=0.4,
                         wspace=None,
                         top=None,
@@ -152,14 +163,72 @@ if __name__ == "__main__":
                         right=None
                         )
 
-    plt.savefig(f'{outDir}/plot_corr-emp-vs-estimation.pdf',
-                bbox_inches='tight')
+    # upper subplot: visual localizer
+    # read the first
+    visDf = pd.read_csv(visResults)
+    axNr = 0
+    # call the plotting function
+    axes[axNr] = plot_subplot(
+        axes[axNr],
+        'PPA localized via visual localizer (Sengupta et al., 2016)',
+        visDf,
+        boxplot=True,
+        legend=True
+    )
 
-    plt.savefig(f'{outDir}/plot_corr-emp-vs-estimation.png',
-                bbox_inches='tight')
+    # plot middle subplot
+    avDf = pd.read_csv(avResults)
+    axNr = 1
+    # call the plotting function
+    axes[axNr] = plot_subplot(
+        axes[axNr],
+        'PPA localized via movie (Häusler et al., 2022)',
+        avDf,
+        boxplot=True,
+        legend=False
+    )
 
-    plt.savefig(f'{outDir}/plot_corr-emp-vs-estimation.svg',
-                bbox_inches='tight')
+    # plot lower subplot
+    aoDf = pd.read_csv(aoResults)
+    axNr = 2
+    # call the plotting function
+    axes[axNr] = plot_subplot(
+        axes[axNr],
+        'PPA localized via audio-description (Häusler et al., 2022)',
+        aoDf,
+        boxplot=True,
+        legend=False
+    )
+
+    # add the Cronbach's a to the subplot
+    # which measure across subjects to calculate?
+    centrTendFunc = np.median  # vs. np.mean
+    # read the results of Cronbachs per stimulus per subject
+    df = pd.read_csv(cronbachs)
+    # pivot the table
+    centrTends = pd.pivot_table(df, index='stimulus', aggfunc=centrTendFunc)
+
+    # loop over the subplots
+    # and add measure of central tendency for predicted PPA
+    for axNr, stimCol in enumerate(list(STIMULICOLORS.items())[1:]):
+        stimulus = stimCol[0]
+        color = stimCol[1]
+
+        # get the value of central tendency of the current loops stimulus
+        centrTend = centrTends.loc[stimulus]["Cronbach's a"]
+        axes[axNr].axhline(y=centrTend,
+                           color=color,
+                           linestyle='--',
+                           alpha=0.6)
+
+    # save the figure
+    os.makedirs(outDir, exist_ok=True)
+
+    extensions = ['pdf', 'png', 'svg']
+    for extension in extensions:
+        fpath = os.path.join(outDir,
+                             f'plot_corr-emp-vs-estimation.{extension}')
+        plt.savefig(fpath, bbox_inches='tight')
 
     plt.show()
 
