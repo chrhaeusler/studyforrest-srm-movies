657713c7793fae4e5c82c4cd08a5040cf5e2f639
Christian O. Häusler <der.haeusler@gmx.net>
2023-07-12 10:33:24 +0200

    set FSL variables

--- code/predict_ppa_venv.sh
+++ code/predict_ppa_venv.sh
@@ -7,5 +7,12 @@ niter=$4
 # activate the virtual environment
 . code/localpython/venv/python37/bin/activate
 
+# FSL v5.0.9 directory
+FSLDIR=~/fsl
+. ${FSLDIR}/etc/fslconf/fsl.sh
+PATH=${FSLDIR}/bin:${PATH}
+export FSLDIR PATH
+. ${FSLDIR}/etc/fslconf/fsl.sh
+
 # call the script
 ./code/predict_ppa.py -indir $indir -model $model -nfeat $nfeat -niter $niter
